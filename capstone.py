bb8a5d3d429d5fe11114c0a70fcac62c5be69e60
RazaneHani <96482183+RazaneHani@users.noreply.github.com>
2022-09-07 13:50:32 +0300

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -390,7 +390,7 @@ if menu_id== 'AML Tool':
     
     use_defo = st.checkbox('Use example Dataset', value= True, key= '77')
     if use_defo:
-        dataset = df1
+        dataset = df
     display_app_header(main_txt='Select Features and Get Transaction Status',
                        sub_txt='')
     sender= (
